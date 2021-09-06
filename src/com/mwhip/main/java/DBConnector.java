f51d395731927945ecba9828368a5e292f44ba95
Samuel Hendryx <samhendryx@gmail.com>
2021-09-06 15:15:50 -0700

    refactor source folder

--- src/com/mwhip/main/java/DBConnector.java
+++ src/com/mwhip/main/java/DBConnector.java
@@ -1,4 +1,4 @@
-package com.mwhip.main.java;
+package main.java;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/com/mwhip/main/java/Main.java
+++ src/com/mwhip/main/java/Main.java
@@ -1,6 +1,10 @@
-package com.mwhip.main.java;
+package main.java;
 
+<<<<<<< HEAD
 import static com.mwhip.main.java.DBConnector.connect;
+=======
+import static main.java.DBConnCheck.connCheck;
+>>>>>>> aecc791 (refactor source folder)
 
 public class Main {
     public static void main (String[] args) {
