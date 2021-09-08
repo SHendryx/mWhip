2c1a1a0ca38edd1495fb0a3be3450815271e3acc
Sam Hendryx <samhendryx@gmail.com>
2021-09-08 12:44:48 -0700

    made changes to db connection call in Main by Sam

--- src/com/mwhip/main/java/Main.java
+++ src/com/mwhip/main/java/Main.java
@@ -1,13 +1,12 @@
 package main.java;
 
-import static main.java.DBConnCheck.connCheck;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main {
     public static void main (String[] args) {
         System.out.println("Starting mWhip...");
-
-        if (connCheck()){
-            System.out.println("Connection to SQLite has been established.");
-        }
+        Connection conn = DBConnection.getINSTANCE();
+        
     }
 }
