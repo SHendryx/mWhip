08d0c8f7615c198b1ff1a938ccedd52aa7dcc71a
Samuel Hendryx <samhendryx@gmail.com>
2021-11-05 10:28:29 -0700

    Added a window with a button.

--- src/com/mwhip/main/java/Main.java
+++ src/com/mwhip/main/java/Main.java
@@ -1,11 +1,19 @@
 package main.java;
 
+import javax.swing.*;
 import java.sql.Connection;
 
 public class Main {
     public static void main (String[] args) {
         System.out.println("Starting mWhip...");
         Connection conn = DBConnection.getINSTANCE();
+
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setVisible(true);
 
     }
 }
